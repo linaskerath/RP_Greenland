e79bd9fcd50b6d26f8db60f2b9c2b1e31a9366c3
linaskerath <lina.skerath@gmail.com>
2022-11-27 14:39:58 +0100

    scope testing and benchmark done

--- Scripts/functions.py
+++ Scripts/functions.py
@@ -3,6 +3,7 @@ import pandas as pd
 import numpy as np
 from sklearn.metrics import mean_squared_error
 import rasterio
+from tqdm import tqdm
 
 def read_and_prep_parquet(path, purpose):
     """
@@ -20,12 +21,12 @@ def read_and_prep_parquet(path, purpose):
     if purpose in ['train', 'test', 'validate']:
         df = df.loc[df['opt_value'] != -1] # remove mask
         df = df.fillna(-1) # fill values to be able to train
-        X = df[['x', 'y', 'mw_value', 'col', 'row', 'v1', 'v2', 'v3', 'v4', 'v6', 'v7', 'v8', 'v9', 'mean']] # v5 is duplicated
+        X = df[['x', 'y', 'mw_value', 'col', 'row', 'v1', 'v2', 'v3', 'v4', 'v6', 'v7', 'v8', 'v9', 'mean', 'elevation_data']] # v5 is duplicated
         y = df[['opt_value']]
         return X, y
     else:
         df = df.fillna(-1) # fill values to be able to train
-        X = df[['x', 'y', 'mw_value', 'col', 'row', 'v1', 'v2', 'v3', 'v4', 'v6', 'v7', 'v8', 'v9', 'mean']] # v5 is duplicated
+        X = df[['x', 'y', 'mw_value', 'col', 'row', 'v1', 'v2', 'v3', 'v4', 'v6', 'v7', 'v8', 'v9', 'mean', 'elevation_data']] # v5 is duplicated
         return X
 
 
@@ -73,3 +74,32 @@ def information(path_to_file):
     print(f'    {data.rio.resolution()}')
     # include min, max, variables, examples..?
     return
+
+
+def save_prediction_tif(X_pred, y_predicted, path_out):
+    """
+    Function to write predictions to .tif.
+    Arguments:
+        X_pred: data to be predicted on.
+        y_predicted: predicted labels in array, or pandas series.
+        path_out: path to save .tif file with file name.
+    Returns: No return, writes data to path.
+    """
+    # join prediction and coordinates (row, col)
+    X_pred['prediction'] = y_predicted 
+
+    # original matrix shape:
+    nan_matrix = np.full((2663, 1462), np.nan)
+
+    for row in tqdm(X_pred.iterrows()): # fix progress bar?
+        row_index = int(row[1]['row'])
+        col_index = int(row[1]['col'])
+        pred_val = row[1]['prediction']
+        nan_matrix[row_index][col_index] = pred_val
+
+    # file to take reference metadata from is interpolated transformed file
+    path_metadata_reference =  r"../Data/microwave-rs/mw_interpolated/2019-07-01_mw.tif"
+
+    convert_to_tif(nan_matrix, path_metadata_reference, path_out)
+
+    return    
