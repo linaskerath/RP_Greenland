b6990c9ec20b6d6624b1b350ad1b7d3aa5203f2c
Nina Nicole Zahn <ninz@itu.dk>
2023-05-09 11:57:56 +0200

    linear regression model without columns X and Y

---
+++ AWS_Scripts/linear_regression_model_no_xy.py
@@ -0,0 +1,44 @@
+import pandas as pd
+from sklearn.linear_model import LinearRegression
+
+pd.options.mode.chained_assignment = None
+
+import functions_training_pipeline as f
+
+import smtplib
+import configparser
+from email.message import EmailMessage
+
+df_path = r"/mnt/volume/AWS_Data/Data/dataframe_model_training/training_data.parquet.gzip"
+
+
+lr = f.Model(model=LinearRegression, name="LinearRegression_NoXY")
+hyperparameters_for_grid = {"fit_intercept": [True]}
+lr.hyperparameters = lr.create_hyperparameter_grid(hyperparameters_for_grid)
+
+data = pd.read_parquet(df_path)
+columns = data.columns.drop(["opt_value", "x", "y"])
+lr.spatial_cv(data, columns, target_normalized=True)
+
+f.save_object(lr)
+
+
+# Read email credentials from config file
+config = configparser.ConfigParser()
+config.read("config.ini")
+sender = config.get("Email", "sender_email")
+password = config.get("Email", "sender_password")
+recipient = config.get("Email", "recipient_email")
+
+# create email
+msg = EmailMessage()
+msg["Subject"] = "AWS Finished"
+msg["From"] = sender
+msg["To"] = recipient
+msg.set_content("AWS instance is done.")
+
+# send email
+with smtplib.SMTP_SSL("smtp.gmx.com", 465) as smtp:
+    smtp.login(sender, password)
+    smtp.send_message(msg)
+    print("Email sent.")
